492b3ac86bf4a0ae9fd3f211ecc0fdce07d28c91
NamDepTrai0201 <27KQrtd.thn@gmail.com>
2022-11-20 11:06:43 +0700

    Code1

---
+++ Admin/SP/index.php
@@ -0,0 +1,87 @@
+<?php
+   seassion_start();
+   if($_SEASSION['username'] == null){
+    header('Location: ../index.php');
+   }
+   $con = mysqli_connect('localhost' , 'root' , '' , 'webadvance') or die('Khong the ket noi toi database');
+   $querry = mysqli_query($con , "select * from admin") or die('Khong the truy van database');
+   $querry1 = mysqli_query($con , "select * from categories") or die('Khong the truy van toi database');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>Danh sach Admin</h3>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Username</th>
+            <th>Password</th>
+            <th>Tac vu</th>
+        </tr>
+        <?php
+        while($row = mysqli_fetch_array($querry , MYSQLI_ASSOC)){
+            $id = $row['id']
+            $user = $row['username']
+            $pass = $row['password']
+        ?>
+        <tr>
+            <td><?php echo $id ?></td>
+            <td><?php echo $user ?></td>
+            <td><?php echo $pass ?></td>
+            <td><a href="admin/sua.php?id=<?php echo $id ;?>">Sua</a></td>
+            <td><a href="admin/xoa.php?id=<?php echo $id ;?>">Xoa</a></td>
+        </tr>
+        <?php }
+        ?>
+    </table>
+    <button type="sumbit" onclick="hienthiAdmin();" id="btn" name="btn"> Them moi </button>;
+
+
+    <h3>===================================================================================================</h3>
+    <h3>Danh sach san pham</h3>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>hinh anh</th>
+            <th>mo ta</th>
+            <th>Gia</th>
+            <th>Tac vu</th>
+        </tr>
+        <?php
+        while($row = mysqli_fetch_array($querry1 , MYSQLI_ASSOC)){
+            $id = $row['id'];
+            $hinhanh = $row['hinhanh'];
+            $mota = $row['mota'];
+            $gia = $row['gia'];
+        ?>
+        <tr>
+            <td><?php echo $id ?></td>
+            <td><?php echo $hinhanh ?></td>
+            <td><?php echo $mota ?></td>
+            <td><?php echo $gia ?></td>
+            <td><a href="SP/sua.php?id=<?php echo $id ;?>">Sua</a></td>
+            <td><a href="SP/xoa.php?id=<?php echo $id ;?>">Xoa</a></td>
+        </tr> 
+        <?php }
+        mysqli_close($con);
+        ?>
+
+    </table>
+    <button type="sumbit" onclick="hienthiSP();" id="btn" name="btn"></i> Them moi</button>
+    <script>
+        function hienthiAdmin(){
+            location.replace("admin/them.php");
+        }
+        function hienthiSP(){
+            location.replace("SP/them.php");
+        }
+    </script>
+</body>
+</html>
