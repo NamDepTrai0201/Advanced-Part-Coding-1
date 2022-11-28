55b00d1891df9183d3d62051ce861ad8d786b970
NamDepTrai0201 <27KQrtd.thn@gmail.com>
2022-11-28 22:53:01 +0700

    UpCodeGoGo

--- Admin/Admin/SP/them.php
+++ Admin/Admin/SP/them.php
@@ -1,56 +1,136 @@
 <?php
-   session_start();
-   if($_SESSION ['username'] == null){
-    header('location: ../../index.php');
-   }
-   $con = mysqli_connect('localhost' , 'root' , '' , 'webadvance') or die('Khong the ket noi toi database');
+session_start();
 
-   if(isset($_POST['btn'])){
-    $imgSrc = $_POST["img-src"];
-    $mota = $_POST["mota"];
-    $gia = $_POST["gia"];
-    if(!$imgSrc || !$mota || !$gia){
-        echo "Vui lòng đăng nhập đầy đủ tên đăng nhập và mật khẩu . <a href='javascript: history.go(-1)'>trở lại</a>";
-        exit;
-    }
-    // $pass = md5($pass);
-    $querry = "INSERT INTO categories (img-src,mota,gia) VALUES ('$imgSrc' , '$mota' , '$gia')";
-    if(mysqli_query($con , $querry)){
-        header('location: ../index.php');
-    }else{
-        $re = "loi them moi" . mysqli_error($con);
-    }
-    mysqli_close($con);
-   }
+if ($_SESSION["username"] == null) {
+  header("Location: ../../index.php");
+}
+
+$con = mysqli_connect("localhost", "root", "", "webadvance") or die("không thể kết nối tới database");
+
+if (isset($_POST["btn"])) {
+  $imgSrc = $_POST["img-src"];
+  $mota = $_POST["mota"];
+  $gia = $_POST["gia"];
+  if (!$imgSrc || !$mota || !$gia) {
+    echo "Vui lòng nhập đầy đủ thông tin. <a href='javascript: history.back();'>Quay lại</a>";
+    exit;
+  }
+
+  $query = "INSERT INTO categories (hinhanh, mota, gia) VALUES ('$imgSrc', '$mota', '$gia')";
+  if (mysqli_query($con, $query)) {
+    header("Location: ../index.php");
+  } else {
+    $re = "lỗi thêm mới" . mysqli_error($con);
+  }
+
+  mysqli_close($con);
+}
 ?>
+
 <!DOCTYPE html>
 <html>
+
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title></title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Add Product</title>
+  <style>
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      font-family: sans-serif;
+      transition: all 0.3s ease;
+    }
+
+    body {
+      padding: 50px;
+    }
+
+    table {
+      width: fit-content;
+      margin-top : 10px
+    }
+
+    input {
+      border: none;
+      outline: none;
+      font-size: 15px;
+      height: 40px;
+      width: 250px;
+      padding-left: 10px;
+      border-bottom: 2px solid #000;
+      background-color: transparent;
+      color: #000;
+      margin-top : 7px;
+      margin-left : 480px
+    }
+
+    button {
+      margin-block: 30px 50px;
+      width: 100px;
+      height: 40px;
+      border: none;
+      outline: none;
+      font-weight: 900;
+      cursor: pointer;
+    }
+
+    .btn-primary {
+      color: #fff;
+      background-color: #000;
+      margin-left : 505px
+    }
+
+    .btn-primary:hover {
+      opacity: 0.7;
+    }
+
+    .btn-secondary {
+      background-color: transparent;
+      border: 2px solid #000;
+
+    }
+
+    .btn-secondary:hover {
+      background-color: #000;
+      color: #fff;
+    }
+
+    .Firsttd{
+        margin-left: 545px;
+        position: absolute;
+        font-size: 0.5cm;
+        margin-top : -13px
+    }
+  </style>
 </head>
+
 <body>
-	<form action='them.php' method='POST'>
-	<table>
-		<tr>
-			<td>Thêm sản phẩm</td>
-		</tr>
-		<tr>
-            <td>image source: </td>
-            <td><input id="img-src" name="img-src" class="form-control" type="text"></td>
-		<tr>
-            <td>Mô tả: </td>
-            <td><input id="mota" name="mota" class="form-control" placeholder="Mô tả" type="text"></td>
-		</tr>
-        <tr>
-            <td>Giá: </td>
-            <td><input id="gia" name="gia" class="form-control" placeholder="Giá" type="text">đ</td>
-        </tr>
-		<td>
-			<button type="submit" id="btn" name="btn"></i> Save</button></td>
-		</tr>
-	</table>
-</form>
+  <form method='POST'>
+    <table>
+      <tr>
+        <td class="Firsttd">Sửa sản phẩm</td>
+      </tr>
+      <tr>
+        <td><input id="img-src" name="img-src" class="form-control" placeholder="Link hình ảnh" type="text"
+            autocomplete="off"></td>
+      </tr>
+      <tr>
+        <td><input id="mota" name="mota" class="form-control" placeholder="Mô tả" type="text" autocomplete="off"></td>
+      </tr>
+      <tr>
+        <td><input id="gia" name="gia" class="form-control" placeholder="Giá" type="text" autocomplete="off">đ</td>
+      </tr>
+      <tr>
+        <td>
+          <button class="btn-primary" type="submit" id="btn" name="btn">Thêm</button>
+          <a href="../index.php"><button class="btn-secondary" type="button">Hủy</button></a>
+        </td>
+      </tr>
+    </table>
+  </form>
 </body>
+
 </html>
--- Admin/Admin/index.php
+++ Admin/Admin/index.php
@@ -17,15 +17,147 @@
     <title>Document</title>
 </head>
 <style>
+    .btn-primary {
+      color: #fff;
+      background-color: #000;
+      /* margin-left : 505px */
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      font-family: sans-serif;
+      transition: all 0.3s ease;
+      background-color: transparent;
+      border: 2px solid #000;
+      text-decoration : none;
+      margin-block: 30px 50px;
+      width: 100px;
+      height: 40px;
+      /* border: none; */
+      outline: none;
+      font-weight: 700;
+      cursor: pointer;
+      color : black;
+    }
+
+    .btn-primary:hover {
+      opacity: 0.7;
+    }
+    .btn-secondary{
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      font-family: sans-serif;
+      transition: all 0.3s ease;
+    }
     img {
      width: 600px;
      height: 200px;
      object-fit: contain;
 }
+    .btn-secondary {
+      background-color: transparent;
+      border: 2px solid #000;
+      text-decoration : none;
+      /* margin-block: 30px 50px; */
+      width: 100px;
+      height: 40px;
+      /* border: none; */
+      outline: none;
+      font-weight: 700;
+      cursor: pointer;
+      color : black;
+    }
+    .btn-secondary:hover {
+      background-color: #000;
+      color: #fff;
+    }
+    /* .trcontainer1{
+        border: 1px solid black;
+        justify-content : space-around;
+    }
+    .mod{
+        margin-left: 200px;
+    } */
+    .Table1{
+        border: 1px solid black;
+        width: 1250px;
+    }
+    .FirstTd{
+        margin-left: 20px;
+        /* position: absolute; */
+    }
+    .SecondTd{
+        margin-left: 180px;
+        position:absolute;
+    }
+    .ThirdTd{
+        margin-left: 510px;
+        position:absolute;
+    }
+    .SuaButton{
+      color: #fff;
+      background-color: #000;
+      margin-left : 900px;
+      /* margin-top: -px; */
+      /* margin: 0; */
+      padding: 0;
+      box-sizing: border-box;
+      font-family: sans-serif;
+      transition: all 0.3s ease;
+      background-color: transparent;
+      border: 1.2px solid #000;
+      /* margin-block: 30px 50px; */
+      width: 50px;
+      /* height: 40px;  */
+      /* border: none; */
+      outline: none;
+      font-weight: 600;
+      cursor: pointer;
+      color : black;
+      position: absolute;
+      text-align: center;
+      text-decoration: none;
+    }
+    .SuaButton:hover {
+      opacity: 0.7;
+    }
+    /* .XoaButton{
+        position: absolute;
+    } */
+    a{
+        text-decoration: none;
+    }
+    .XoaButton{
+      margin-left : 1020px;
+      position: absolute;
+      background-color: transparent;
+      border: 1.2px solid #000;
+      box-sizing: border-box;
+      text-decoration : none;
+      /* margin-block: 30px 50px; */
+      width: 50px;
+      /* height: 40px; */
+      /* border: none; */
+      outline: none;
+      font-weight: 600;
+      cursor: pointer;
+      color : black;
+      text-align: center;
+      text-decoration: none;
+      padding : 0;
+      font-family: sans-serif;
+    }
+    .XoaButton:hover {
+      background-color: #000;
+      color: #fff;
+    }
+    .ListAdmin , .ListSanpham{
+        text-align:center;
+    }
 </style>
 <body>
-    <h3>Danh sach Admin</h3>
-    <table>
+    <h3 class="ListAdmin">Danh sach Admin</h3>
+    <table class="Table1">
         <tr>
             <th>ID</th>
             <th>Username</th>
@@ -39,11 +171,11 @@
             $pass = $row['password'];
         ?>
         <tr>
-            <td><?php echo $id ?></td>
-            <td><?php echo $user ?></td>
-            <td><?php echo $pass ?></td>
-            <td><a href="admin/sua.php?id=<?php echo $id ;?>">Sua</a></td>
-            <td><a href="admin/xoa.php?id=<?php echo $id ;?>">Xoa</a></td>
+            <td class="FirstTd"><?php echo $id ?></td>
+            <td class="SecondTd"><?php echo $user ?></td>
+            <td class="ThirdTd"><?php echo $pass ?></td>
+            <td class="SuaButton"><a href="admin/sua.php?id=<?php echo $id ;?>">Sửa</a></td>
+            <td class="XoaButton"><a href="admin/xoa.php?id=<?php echo $id ;?>">Xóa</a></td>
         </tr>
         <?php }
         ?>
@@ -51,10 +183,10 @@
     <button type="sumbit" onclick="hienthiAdmin();" id="btn" name="btn"> Them moi </button>;
 
 
-    <h3>===================================================================================================</h3>
-    <h3>Danh sach san pham</h3>
+    <h3>======================================================================================================================</h3>
+    <h3 class="ListSanpham">Danh sach san pham</h3>
     <table>
-        <tr>
+        <tr class="trcontainer1">
             <th>ID</th>
             <th>hinh anh</th>
             <th>mo ta</th>
@@ -74,8 +206,8 @@
                 <img src="<?php echo $hinhanh ?>" alt="<?php echo $mota ?>"></td>
             <td><?php echo $mota ?></td>
             <td><?php echo $gia ?></td>
-            <td><a href="SP/sua.php?id=<?php echo $id ;?>">Sua</a></td>
-            <td><a href="SP/xoa.php?id=<?php echo $id ;?>">Xoa</a></td>
+            <td><a href="SP/sua.php?id=<?php echo $id ;?>" class="btn-primary">Sua</a></td>
+            <td><a href="SP/xoa.php?id=<?php echo $id ;?>" class="btn-secondary">Xoa</a></td>
         </tr> 
         <?php }
         mysqli_close($con);
